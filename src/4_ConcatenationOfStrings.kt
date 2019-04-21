5bb91781f029971657ec47d16557859ea0dfad88
deepshooter <avinashm720@gmail.com>
2019-04-21 20:36:26 +0530

    Adding new files

---
+++ src/4_ConcatenationOfStrings.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    demo4()
+}
+
+fun demo4() {
+
+    val firstName = "Avinash"
+    val lastName = "Mishra"
+    val message = "My name is: $firstName $lastName"
+
+    print(message)
+}
