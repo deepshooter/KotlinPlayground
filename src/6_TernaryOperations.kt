aaef42b15c4e1e45428ec76362feaef6c4034b12
deepshooter <avinashm720@gmail.com>
2019-04-23 16:58:35 +0530

    Adding new files

---
+++ src/6_TernaryOperations.kt
@@ -0,0 +1,18 @@
+// Ternary Operations
+
+fun main(args: Array<String>) {
+
+    demo6()
+
+}
+l
+fun demo6() {
+
+    val x = 10
+    val text = if (x > 5)
+        "x > 5"
+    else "x <= 5"
+    val message: String? = null
+    print(message ?: text)
+
+}
