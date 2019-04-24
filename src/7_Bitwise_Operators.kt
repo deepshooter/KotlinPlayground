46550f5d3ef4b3fdb3e7f5f664c521168720f638
deepshooter <avinashm720@gmail.com>
2019-04-24 14:04:41 +0530

    Adding new files

---
+++ src/7_Bitwise_Operators.kt
@@ -0,0 +1,21 @@
+// Bitwise Operators
+
+fun main(args: Array<String>) {
+
+    demo7()
+
+}
+
+fun demo7() {
+
+    val a = true
+    val b = false
+
+
+    val andResult = a and b
+    val orResult = a or b
+
+    println(andResult)
+    println(orResult)
+
+}
