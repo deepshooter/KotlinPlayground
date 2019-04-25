c9810044b2986ee5f55da8db7626d045d2f09a29
deepshooter <avinashm720@gmail.com>
2019-04-25 09:48:03 +0530

    Adding new files

---
+++ src/9_SwitchCase.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    demo9()
+
+}
+
+fun demo9() {
+
+    var score = 7
+
+    var grade = when (score) {
+        9, 10 -> "Excellent"
+        in 6..8 -> "Good"
+        4, 5 -> "OK"
+        in 1..3 -> "Fail"
+        else -> "Fail"
+    }
+
+    print(grade)
+
+
+}
