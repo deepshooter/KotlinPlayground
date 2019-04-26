828622559d8b90b4c306ed2fbe556297440c4835
deepshooter <avinashm720@gmail.com>
2019-04-26 11:30:30 +0530

    Adding new files

---
+++ src/10_ForLoopsAndCollections.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+
+    demo10()
+
+}
+
+
+fun demo10() {
+
+    val listOfNumber = listOf(1, 2, 3, 4)
+    val keyValueMap = mapOf(
+        1 to "Android",
+        2 to "Java",
+        3 to "Kotlin"
+    )
+
+    for (i in 1..10) {
+    }
+    for (i in 1 until 10) {
+    }
+    for (i in 10 downTo 0) {
+    }
+    for (i in 1..10 step 2) {
+    }
+    for (i in 10 downTo 0 step 2) {
+    }
+    for (item in listOfNumber) {
+        println(item)
+    }
+    for ((key, value) in keyValueMap) {
+        println("$key and $value")
+    }
+
+}
