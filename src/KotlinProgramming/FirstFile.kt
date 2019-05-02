b8bd72abc0969ca2535dc8ec2feab5c8632cf662
deepshooter <avinashm720@gmail.com>
2019-05-02 16:12:06 +0530

    Adding new files

--- src/KotlinProgramming/FirstFile.kt
+++ src/KotlinProgramming/FirstFile.kt
@@ -1,8 +1,26 @@
 package KotlinProgramming
 
 
-fun main(args:Array<String>){
+/*
+ *
+ * This is the main function.Entry point of the application
+ *
+ */
 
-    print("Hello World !")
-    
+
+fun main(args: Array<String>) {
+
+    println("Hello World !")
+    println("Hello Kotlin !")
+    println(10 - 5)
+    println(10 / 9)
+
 }
+
+//This is inline comment
+
+/*
+ * This is comment line 1
+ * This is comment line 2
+ *
+ */
