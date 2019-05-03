de103828490f5d1314097b73f5ef6c293471c4e2
deepshooter <avinashm720@gmail.com>
2019-05-03 09:51:27 +0530

    Adding new files

--- src/KotlinProgramming/FirstFile.kt
+++ src/KotlinProgramming/FirstFile.kt
@@ -15,7 +15,33 @@ fun main(args: Array<String>) {
     println(10 - 5)
     println(10 / 9)
 
+    varDemo()
+    valDemo()
+
 }
+
+fun varDemo() {
+
+    var myString = "Hello...." //Mutable String
+    var myNumber = 10
+    var myDecimal = 1.0
+
+    var myOtherString: String
+    myOtherString = "Hello Again.."
+
+    println(myString)
+    println(myNumber)
+    println(myDecimal)
+    println(myOtherString)
+
+}
+
+
+fun valDemo() {
+    val myTestString = "My Constant String !" //Immutable String
+    println(myTestString)
+}
+
 
 //This is inline comment
 
