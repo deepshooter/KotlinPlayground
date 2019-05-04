cb94a8df96b3b805f43d4c79ea3eab8a28388c0b
deepshooter <avinashm720@gmail.com>
2019-05-04 19:23:18 +0530

    Adding new files

---
+++ src/KotlinProgramming/basic/MyFile.kt
@@ -0,0 +1,8 @@
+package KotlinProgramming.basic
+
+fun main(args: Array<String>) {
+
+    var person = Person("Bob")
+    person.display()
+    
+}
---
+++ src/KotlinProgramming/basic/Person.kt
@@ -0,0 +1,10 @@
+package KotlinProgramming.basic
+
+class Person(var name: String) {
+
+    fun display() {
+
+        println("The name of the person is ${name}")
+    }
+
+}
