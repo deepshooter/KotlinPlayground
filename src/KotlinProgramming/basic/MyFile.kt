b24aa3daf24a757890d28c50af6accbef1d51005
deepshooter <avinashm720@gmail.com>
2019-05-06 13:37:58 +0530

    Adding new files

--- src/KotlinProgramming/basic/MyFile.kt
+++ src/KotlinProgramming/basic/MyFile.kt
@@ -2,7 +2,7 @@ package KotlinProgramming.basic
 
 fun main(args: Array<String>) {
 
-    var person = Person("Bob")
+    var person = Person("John")
     person.display()
 
 }
--- src/KotlinProgramming/basic/Students.kt
+++
@@ -1,4 +0,0 @@
-package KotlinProgramming.basic
-
-class Students {
-}
