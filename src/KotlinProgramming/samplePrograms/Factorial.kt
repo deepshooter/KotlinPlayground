15b2f48b780844f7e71ce85e4485eb8913a9bc5d
deepshooter <avinashm720@gmail.com>
2019-05-10 15:04:28 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Factorial.kt
@@ -0,0 +1,13 @@
+package KotlinProgramming.samplePrograms
+
+fun main(args: Array<String>) {
+
+    val num = 10
+    var factorial: Long = 1
+    for (i in 1..num) {
+        // factorial = factorial * i;
+        factorial *= i.toLong()
+    }
+    println("Factorial of $num = $factorial")
+    
+}
