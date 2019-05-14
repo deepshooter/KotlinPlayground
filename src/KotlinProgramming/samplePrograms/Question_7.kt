be450812a5797282699afbb386a883b6044fa5fc
deepshooter <avinashm720@gmail.com>
2019-05-14 09:46:58 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_7.kt
@@ -0,0 +1,18 @@
+package KotlinProgramming.samplePrograms
+
+//Sum of Natural Numbers using for loop
+
+fun main(args: Array<String>) {
+
+    val num = 100
+    var sum = 0
+
+    for (i in 1..num) {
+        // sum = sum+i;
+        sum += i
+    }
+
+    println("Sum = $sum")
+    
+}
+
