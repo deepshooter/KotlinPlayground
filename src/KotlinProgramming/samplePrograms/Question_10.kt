a838c34329937c0aba2df23e88a4b1f8f5333f20
deepshooter <avinashm720@gmail.com>
2019-05-16 16:42:31 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_10.kt
@@ -0,0 +1,15 @@
+package KotlinProgramming.samplePrograms
+
+//Check if a Number is Positive or Negative using when expression
+
+fun main(args: Array<String>) {
+
+    val number = -12.3
+
+    when {
+        number < 0.0 -> println("$number is a negative number.")
+        number > 0.0 -> println("$number is a positive number.")
+        else -> println("$number is 0.")
+    }
+    
+}
