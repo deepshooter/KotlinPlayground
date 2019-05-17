cad065317feb4a09f4886e9e58d15f219d2ee119
deepshooter <avinashm720@gmail.com>
2019-05-17 10:22:25 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_12.kt
@@ -0,0 +1,25 @@
+package KotlinProgramming.samplePrograms
+
+//Display Prime Numbers Between two Intervals
+
+fun main(args: Array<String>) {
+    var low = 20
+    val high = 50
+
+    while (low < high) {
+        var flag = false
+
+        for (i in 2..low / 2) {
+            // condition for nonprime number
+            if (low % i == 0) {
+                flag = true
+                break
+            }
+        }
+
+        if (!flag)
+            print("$low ")
+
+        ++low
+    }
+}
