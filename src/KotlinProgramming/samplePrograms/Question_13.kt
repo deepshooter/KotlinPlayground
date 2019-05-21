6ace21a21e0c914cf30c5a8e92fd35e0097535ad
deepshooter <avinashm720@gmail.com>
2019-05-21 12:44:35 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_13.kt
@@ -0,0 +1,14 @@
+package KotlinProgramming.samplePrograms
+
+//Factors of a Positive Integer
+
+fun main(args: Array<String>) {
+    val number = 60
+
+    print("Factors of $number are: ")
+    for (i in 1..number) {
+        if (number % i == 0) {
+            print("$i ")
+        }
+    }
+}
