a31c98323a562c226a00706c20f46b9bec0cf032
deepshooter <avinashm720@gmail.com>
2019-05-21 12:47:08 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_14.kt
@@ -0,0 +1,16 @@
+package KotlinProgramming.samplePrograms
+
+//Reverse a number in Kotlin
+
+fun main(args: Array<String>) {
+    var num = 1234
+    var reversed = 0
+
+    while (num != 0) {
+        val digit = num % 10
+        reversed = reversed * 10 + digit
+        num /= 10
+    }
+
+    println("Reversed Number: $reversed")
+}
