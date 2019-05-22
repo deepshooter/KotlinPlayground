3275f12dd47c6621501398692ccfd7c0d8b1ee91
deepshooter <avinashm720@gmail.com>
2019-05-22 11:16:55 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_15.kt
@@ -0,0 +1,13 @@
+package KotlinProgramming.samplePrograms
+
+//Program to Remove All Whitespaces
+
+fun main(args: Array<String>) {
+
+    var sentence = "T    his is b  ett     er."
+    println("Original sentence: $sentence")
+
+    sentence = sentence.replace("\\s".toRegex(), "")
+    println("After replacement: $sentence")
+
+}
