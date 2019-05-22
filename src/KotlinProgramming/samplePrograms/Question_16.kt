e66750504a6a8d78066ab95fc7ee054bc45074d9
deepshooter <avinashm720@gmail.com>
2019-05-22 11:20:16 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_16.kt
@@ -0,0 +1,16 @@
+package KotlinProgramming.samplePrograms
+
+// Program to Reverse a Sentence Using Recursion
+
+fun main(args: Array<String>) {
+    val sentence = "Go work"
+    val reversed = reverse(sentence)
+    println("The reversed sentence is: $reversed")
+}
+
+fun reverse(sentence: String): String {
+    if (sentence.isEmpty())
+        return sentence
+
+    return reverse(sentence.substring(1)) + sentence[0]
+}
