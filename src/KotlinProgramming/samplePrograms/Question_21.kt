90ee9b13c4a52db3f13aff2decc180186828103c
deepshooter <avinashm720@gmail.com>
2019-05-27 08:48:57 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_21.kt
@@ -0,0 +1,14 @@
+package KotlinProgramming.samplePrograms
+
+//Convert Array to Set
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val array = arrayOf("a", "b", "c")
+    val set = HashSet(Arrays.asList(*array))
+
+    println("Set: $set")
+
+}
