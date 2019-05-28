1143c592b805d1244e93f34a16ba34294f243098
deepshooter <avinashm720@gmail.com>
2019-05-28 17:08:45 +0530

    Adding new files

---
+++ src/KotlinProgramming/samplePrograms/Question_24.kt
@@ -0,0 +1,14 @@
+package KotlinProgramming.samplePrograms
+
+//Convert char array to String
+
+fun main(args: Array<String>) {
+
+    val ch = charArrayOf('a', 'e', 'i', 'o', 'u')
+
+    val st = String(ch)
+    val st2 = String(ch)
+
+    println(st)
+    println(st2)
+}
